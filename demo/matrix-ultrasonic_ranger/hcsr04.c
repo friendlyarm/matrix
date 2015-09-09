70b759f597b92f39a579f086445479a74d90e10e
wuweidong <ericrock@foxmail.com>
2015-09-09 18:20:45 +0800

    support matrix-ultrasonic_ranger

--- demo/matrix-ultrasonic_ranger/hcsr04.c
+++ demo/matrix-ultrasonic_ranger/hcsr04.c
@@ -5,15 +5,13 @@
 int main(int argc, char ** argv)
 {
     int distance = -1;
-    
-    int echoPin = GPIO_PIN1;
-    int triggerPin = GPIO_PIN2;
-    
-    if (Hcsr04Init(echoPin, triggerPin) == -1) {
+
+    int pin = GPIO_PIN1;
+
+    if (Hcsr04Init(pin) == -1) {
         printf("Fail to init hcsr04\n");
     }
-    
-    if (Hcsr04Read(&distance)==0 && distance != -1) {
+    if (Hcsr04Read(&distance) != -1) {
         printf("Get distance: %3d cm\n", distance);
     } else {
         printf("Faid to get distance\n");
--- lib/iio.c
+++ lib/iio.c
@@ -87,7 +87,7 @@ static int Hcsr04Write(char* fileName, char* buff) {
     return 0;
 }
 
-EXPORT int Hcsr04Init(int echoPin, int triggerPin) 
+EXPORT int Hcsr04Init(int Pin) 
 {
     clearLastError();
     struct HCSR04_resource res;
@@ -96,8 +96,7 @@ EXPORT int Hcsr04Init(int echoPin, int triggerPin)
     memset(hcsr04Path, 0, FILE_PATH_LENGTH);
     strcpy(hcsr04Path, HCSR04_PATH);
 
-    res.echoPin = echoPin;
-    res.triggerPin = triggerPin;
+    res.Pin = Pin;
     
     char *resStr = (char *)&res;    
     if (Hcsr04Write(hcsr04Path, resStr) == -1) {
@@ -113,8 +112,7 @@ EXPORT void Hcsr04DeInit()
 {
     clearLastError();
     struct HCSR04_resource res;
-    res.echoPin = -1;
-    res.triggerPin = -1;
+    res.Pin = -1;
 
     char *hcsr04Path = (char *) malloc(FILE_PATH_LENGTH);
     memset(hcsr04Path, 0, FILE_PATH_LENGTH);
@@ -131,25 +129,11 @@ EXPORT int Hcsr04Read(int *distance)
 {
     clearLastError();
     int ret = HCSR04_MAX_DISTANCE + 1;
-    int errCount = -1;
-
     char *hcsr04Path = (char *) malloc(FILE_PATH_LENGTH);
     memset(hcsr04Path, 0, FILE_PATH_LENGTH);
     strcpy(hcsr04Path, HCSR04_PATH);
-
-    while(ret > HCSR04_MAX_DISTANCE && errCount < 5) {
-        ret = readIntValueFromFile(hcsr04Path);
-        errCount++;
-    }
-
-    if (errCount == 5) {
-        ret = -1;
-        setLastError("Invalid hcsr04 data");
-    } else {            
-        *distance = (int)(ret / HCSR04_PER_METRE);
-        ret = 0;
-    }
-
+    ret = readIntValueFromFile(hcsr04Path);
+    *distance = (int)(ret / HCSR04_PER_METRE);
     free(hcsr04Path);
     return ret;
 }
