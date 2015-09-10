fd51084bb93a266c4f330ea5dc842ff53f2ee90d
wuweidong <ericrock@foxmail.com>
2015-09-10 15:23:40 +0800

    remove magic number in adxl34x demo

--- demo/matrix-3_axis_digital_accelerometer/matrix-3_axis_digital_accelerometer.c
+++ demo/matrix-3_axis_digital_accelerometer/matrix-3_axis_digital_accelerometer.c
@@ -3,16 +3,17 @@
 #include <string.h>
 #include "libfahw.h"
 
+#define BUF_SIZE 32
+
 int main(int argc, char ** argv) 
 {
-    char *position = (char *) malloc(32);
-    memset(position, 0, 32);
+    char position[BUF_SIZE];
+    memset(position, 0, BUF_SIZE);
     
     if (adxl34xRead(position) > 0) {
         printf("Get position: %s", position);
     } else {
         printf("Fail to get position\n");        
     }
-    free(position);
     return 0;
 }
