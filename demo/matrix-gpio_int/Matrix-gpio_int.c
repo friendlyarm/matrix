4bc96d2aae9f282e64eeb2bcb62aa9a38a0398fb
wwd <ericrock@foxmail.com>
2016-05-24 18:47:20 +0800

    sensor: all gpio int sensor use one demo

--- demo/matrix-gpio_int/Matrix-gpio_int.c
+++ demo/matrix-gpio_int/Matrix-gpio_int.c
@@ -5,7 +5,7 @@
 
 #define DRIVER_MODULE "matrix_gpio_int"
 
-static struct sensor brSwitch[] = {
+static struct sensor dev[] = {
         {
                 GPIO_PIN(7),
                 IRQ_TYPE_EDGE_FALLING,
@@ -16,7 +16,7 @@ int devFD = -1;
 void intHandler(int signNum)
 {
     if (signNum == SIGINT) {
-        printf("Quit reading\n");
+        printf("Quit waiting\n");
         sensorDeinit(devFD);
         system("rmmod "DRIVER_MODULE);
     }
@@ -27,31 +27,31 @@ int main(int argc, char ** argv)
 {
     int i, board;
     int retSize = -1;
-    char value[ARRAY_SIZE(brSwitch)];
+    char value[ARRAY_SIZE(dev)];
 
-    if (argc == 2) {
-        brSwitch[0].pin = atoi(argv[1]);
-    }
-    
-    printf("Using GPIO_PIN(%d)\n", brSwitch[0].pin);
     if ((board = boardInit()) < 0)
         printf("Fail to init board\n");
+    
+    if (argc == 2) {
+        dev[0].pin = atoi(argv[1]);
+    }
+    printf("Using GPIO_PIN(%d)\n", dev[0].pin);
     system("modprobe "DRIVER_MODULE);
     signal(SIGINT, intHandler);
     if (board == BOARD_NANOPI_T2)
-        brSwitch[0].pin = GPIO_PIN(15);
-    if ((devFD =sensorInit(brSwitch, ARRAY_SIZE(brSwitch))) == -1) {
+        dev[0].pin = GPIO_PIN(15);
+    if ((devFD =sensorInit(dev, ARRAY_SIZE(dev))) == -1) {
         printf("Fail to init sensor\n");
         return -1;
     }
-    printf("Lean the switch...\n");
-    if ((retSize = sensorRead(devFD, value, ARRAY_SIZE(brSwitch))) == -1) {
+    printf("Waiting event...\n");
+    if ((retSize = sensorRead(devFD, value, ARRAY_SIZE(dev))) == -1) {
         printf("Fail to read sensors\n");
     }
     if (retSize > 0) {
         i = 0;
         for (i=0; i<retSize; i++) {
-            printf("Switch[%d]:%d\n", i, value[i]);
+            printf("dev[%d] value: %d\n", i, value[i]);
         }
     }
     sensorDeinit(devFD);
